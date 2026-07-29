0ff9724c840966c1e3696c0d00ba819ac2bfdf45
Angelicacsares <angelcsjdlu@gmail.com>
2026-07-29 19:18:42 -0300

    Atualizar o desenvolvedor.js

--- desenvolvedor.js
+++ desenvolvedor.js
@@ -1,21 +1,39 @@
 // ======================================================
-// ANIMALLOVER - DESENVOLVEDOR
+// DESENVOLVEDOR
 // ======================================================
+
+function limparClientes(){
+
+    if(confirm("Deseja apagar todos os clientes e pets?")){
+
+        localStorage.removeItem("clientes");
+        localStorage.removeItem("pets");
+        localStorage.removeItem("agendamentos");
+
+        clientes = [];
+
+        abrirClientes();
+
+    }
+
+}
+
+function limparPets(){
+
+    // Futuramente
+}
 
 function limparAgenda(){
 
     // Futuramente
-
 }
 
 function exportarBackup(){
 
     // Futuramente
-
 }
 
 function importarBackup(){
 
     // Futuramente
-
 }
