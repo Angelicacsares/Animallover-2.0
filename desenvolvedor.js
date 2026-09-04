6000bc968e493efa4c68bf2d3d014023f1b41ac0
Angelicacsares <angelcsjdlu@gmail.com>
2026-09-03 21:18:53 -0300

    Remove limparClientes and limparPets functions
    Removed limparClientes and limparPets functions, leaving limparAgenda and importarBackup for future implementation.

--- desenvolvedor.js
+++ desenvolvedor.js
@@ -2,27 +2,6 @@
 // DESENVOLVEDOR
 // ======================================================
 
-function limparClientes(){
-
-    if(confirm("Deseja apagar todos os clientes e pets?")){
-
-        localStorage.removeItem("clientes");
-        localStorage.removeItem("pets");
-        localStorage.removeItem("agendamentos");
-
-        clientes = [];
-
-        abrirClientes();
-
-    }
-
-}
-
-function limparPets(){
-
-    // Futuramente
-}
-
 function limparAgenda(){
 
     // Futuramente
